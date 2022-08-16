ef665970691a962eadc1591d2148217f711026de
Wheeeeeeeeels <ada_wheels@163.com>
2022-08-17 01:13:39 +0800

    Update res.sql

--- tinysql/proj1/part1/res.sql
+++ tinysql/proj1/part1/res.sql
@@ -1,3 +1,5 @@
+### 1. SQL Grammer
+
 ## Revising the Select Query I (10%)
 ## my solution
-select * from CITY where COUNTRYCODE = 'USA' and POPULATION > 100000
+SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000
