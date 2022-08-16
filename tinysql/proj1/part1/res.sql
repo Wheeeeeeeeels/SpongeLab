0c97cfaec32336a0fa49192d304d6d3df5a4c80d
Wheeeeeeeeels <ada_wheels@163.com>
2022-08-17 01:17:28 +0800

    Update res.sql

--- tinysql/proj1/part1/res.sql
+++ tinysql/proj1/part1/res.sql
@@ -3,3 +3,6 @@
 ## Revising the Select Query I (10%)
 ## my solution
 SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000
+
+## Revising the Select Query I (10%)
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000
