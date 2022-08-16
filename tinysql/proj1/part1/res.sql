2227df2e46fc0392050e8fb4847675f6f341f6af
Wheeeeeeeeels <ada_wheels@163.com>
2022-08-17 01:25:48 +0800

    Update res.sql

--- tinysql/proj1/part1/res.sql
+++ tinysql/proj1/part1/res.sql
@@ -6,3 +6,12 @@ SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000
 
 ## Revising the Select Query I (10%)
 SELECT NAME FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000
+
+## Revising Aggregations - The Count Function (10%)
+SELECT COUNT(*) FROM CITY WHERE POPULATION > 100000
+
+## Revising Aggregations - The Sum Function (10%)
+SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT='California'
+
+## Revising Aggregations - Averages (10%)
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California'
